4b67d705815bfed362cbbafdfb51c52145d6643a
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2024-10-15 23:40:22 -0700

    quick save

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,32 @@
-import colors from 'tailwindcss/colors';
-import starlightPlugin from '@astrojs/starlight-tailwind';
+import starlightPlugin from "@astrojs/starlight-tailwind";
+
+const accent = {
+  200: "#aec9ff",
+  600: "#0222f4",
+  900: "#0a277a",
+  950: "#0b1f52",
+};
+const gray = {
+  100: "#f6f6f6",
+  200: "#eeeeee",
+  300: "#c2c2c2",
+  400: "#8b8b8b",
+  500: "#585858",
+  700: "#383838",
+  800: "#272727",
+  900: "#181818",
+};
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
-		},
-	},
-	plugins: [starlightPlugin()],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        accent,
+        gray,
+      },
+    },
+  },
+  plugins: [starlightPlugin()],
 };
